2bcfa43b4e01c32e215ae31edd6365401012e39e
Bohdan Danishevsky <dbn@aminis.com.ua>
2022-09-19 21:15:35 +0100

    Fix for wrong commit

--- picow/002_battery_powered_wireless_sensor/battery_info.py
+++ picow/002_battery_powered_wireless_sensor/battery_info.py
@@ -1,217 +1,73 @@
-import gc
-import os
-import sys
-import time
-
-import bme280
 import machine
-import network
-import ntptime
-import ubinascii
-import ujson
-from ucollections import namedtuple
-from umqtt.simple2 import MQTTClient
-
-import consts
-import functools
-import secrets
-from battery_info import PicoWBatteryInfo
-from internal_temperature_sensor import InternalTemperatureSensor
-from misc import get_machine_unique_id
-from retry_exception import retry_exception
-
-try:
-    from typing import Callable, Dict, Tuple
-except ImportError:
-    pass
-
-Bme280Data = namedtuple("Bme280Data", ("temperature", "pressure", "humidity"))
-
-i2c = machine.I2C(0, sda=machine.Pin(0), scl=machine.Pin(1), freq=400_000)
-bme = bme280.BME280(i2c=i2c)
 
 
-@retry_exception(attempts=3, delay_seconds=5)
-def setup_current_timestamp() -> None:
-    try:
-        ntptime.settime()
-    except Exception as e:
-        print(f"Error configuring current timestamp: {e}")
-        raise
+class PicoWBatteryInfo(object):
+    CONVERSION_FACTOR = 3 * 3.3 / 65535
 
+    # Full battery voltage (volts)
+    FULL_BATTERY = 4.2
 
-def get_current_timestamp_iso() -> str:
-    localtime = time.localtime()
-    return f"{localtime[0]}-{localtime[1]:02}-{localtime[2]:02} {localtime[3]:02}:{localtime[4]:02}:{localtime[5]:02}"
+    # Low battery voltage (volts)
+    EMPTY_BATTERY = 2.8
 
+    def __init__(
+            self,
+            vsys_pin: int = 29
+    ):
+        self.vsys_pin = vsys_pin
 
-@retry_exception(attempts=3, delay_seconds=5)
-def read_bme280_values() -> Bme280Data:
-    return Bme280Data(
-        temperature=bme.values[0],
-        pressure=bme.values[1],
-        humidity=bme.values[2]
-    )
+        self._vsys_adc = None
+        self._spi_cs_control = None  # Controls SPI CS used by Wi-Fi module
+        self._initialized = False
 
+    @property
+    def current_voltage(self) -> float:
+        try:
+            if not self._initialized:
+                self._init()
 
-@retry_exception(attempts=3, delay_seconds=5)
-def connect_to_wifi() -> Tuple[str, str]:
-    """
-    Connects to Wi-Fi and returns IP and adapter MAC address
-    """
+            self._disable_wifi()
 
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
+            return self._vsys_adc.read_u16() * PicoWBatteryInfo.CONVERSION_FACTOR
+        finally:
+            self._enable_wifi()
 
-    print(f"Trying connect to WiFi (SSID = {secrets.WIFI_SSID})")
-    wlan.connect(secrets.WIFI_SSID, secrets.WIFI_PASSWORD)
-
-    MAX_OPERATION_TIME_MSECS = 60 * 1000
-    CONNECTION_POLLING_INTERVAL_SECS = 1
-
-    ticks_start = time.ticks_ms()
-    while not wlan.isconnected():
-        print("Waitinig for WiFi connection ... ")
-        # led.toggle()
-        now = time.ticks_ms()
-        if time.ticks_diff(now, ticks_start) > MAX_OPERATION_TIME_MSECS:
-            raise Exception("Can't connect to Wi-Fi")
-
-        time.sleep(CONNECTION_POLLING_INTERVAL_SECS)
-
-    ip = wlan.ifconfig()[0]
-    print(f"Connected on {ip}")
-    print(f"Full config: {wlan.ifconfig()}")
-
-    mac_address = ubinascii.hexlify(network.WLAN().config('mac'), ':').decode()
-    return ip, mac_address
-
-
-@retry_exception(attempts=5, delay_seconds=5)
-def mqtt_connect() -> MQTTClient:
-    print(f"Trying connect to MQTT server - {secrets.MQTT_SERVER}")
-    client = MQTTClient(
-        secrets.MQTT_CLIENT_ID,
-        secrets.MQTT_SERVER,
-        user=secrets.MQTT_USER,
-        password=secrets.MQTT_PASSWORD,
-        keepalive=60,
-        ssl=False,
-        ssl_params={}
-    )
-    client.connect(clean_session=True)
-    print(f'Connected to MQTT Broker {secrets.MQTT_SERVER}...')
-    return client
-
-
-def get_fs_free_space_in_bytes() -> int:
-    fs_stat = os.statvfs("/")
-    assert fs_stat[0] == fs_stat[1], "Unknown FS stat response, sorry"
-    return fs_stat[0] * fs_stat[3]
-
-
-def get_python_version() -> str:
-    v = sys.version_info
-    return f"{v[0]}.{v[1]}.{v[2]}"
-
-
-def enrich_metadata(
-        packet: Dict,
-        mac_address: str,
-        internal_temp_sensor: InternalTemperatureSensor,
-        current_voltage: float = None,
-        charge_percentage: float = None
-):
-    """
-    Enriches payload with additional metadata
-    """
-
-    packet["metadata"] = {
-        "wifi_mac_address": mac_address,
-        "machine_unique_id": get_machine_unique_id(),
-        "measurement_time": get_current_timestamp_iso(),
-        "machine_metrics": {
-            "python_version": get_python_version(),
-            "cpu_temperature": internal_temp_sensor.current_temperature(),
-            "mem_free": gc.mem_free(),
-            "frequency": machine.freq(),
-            "flash_free_space_bytes": get_fs_free_space_in_bytes(),
-            "power": {
-                "current_voltage": current_voltage,
-                "charge_percentage": charge_percentage
-            }
-        }
-    }
-
-
-def send_measurements(client: MQTTClient, enricher: Callable) -> None:
-    bme280_data = read_bme280_values()
-
-    payload = {
-        "payload": {
-            "bme280": {
-                "temperature": bme280_data.temperature,
-                "pressure": bme280_data.pressure,
-                "humidity": bme280_data.humidity
-            },
-        },
-
-    }
-    enricher(payload)
-    payload = ujson.dumps(payload)
-
-    print(f"Sending measurements via MQTT: {payload}")
-    client.publish(secrets.MQTT_TOPIC_PUB, msg=payload)
-
-
-def deactivate_wifi() -> None:
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(False)
-
-
-def main():
-    try:
-        print(f"Current timestamp is: {get_current_timestamp_iso()}")
-
-        print("Measuring battery charge level")
-        battery_info = PicoWBatteryInfo()
-        current_voltage = battery_info.current_voltage
-        charge_percentage = battery_info.calculate_percentage(current_voltage)
-        print(f"Current battery voltage level: {current_voltage}")
-        print(f"Battery charge percentage level: {charge_percentage}")
-
-        print("Connecting to Wi-Fi")
-        _, mac_address = connect_to_wifi()
-
-        print("Confuguring current timestamp")
-        setup_current_timestamp()
-
-        client = mqtt_connect()
-
-        internal_temp_sensor = InternalTemperatureSensor()
-        enricher = functools.partial(
-            enrich_metadata,
-            mac_address=mac_address,
-            internal_temp_sensor=internal_temp_sensor,
-            current_voltage=current_voltage,
-            charge_percentage=charge_percentage
+    @property
+    def charge_percentage(self) -> float:
+        percentage = 100 * (
+                (self.current_voltage - PicoWBatteryInfo.EMPTY_BATTERY) /
+                (PicoWBatteryInfo.FULL_BATTERY - PicoWBatteryInfo.EMPTY_BATTERY)
         )
+        if percentage > 100:
+            percentage = 100.00
 
-        send_measurements(
-            client,
-            enricher=enricher
+        return percentage
+
+    def calculate_percentage(self, current_voltage: float) -> float:
+        percentage = 100 * (
+                (current_voltage - PicoWBatteryInfo.EMPTY_BATTERY) /
+                (PicoWBatteryInfo.FULL_BATTERY - PicoWBatteryInfo.EMPTY_BATTERY)
         )
+        if percentage > 100:
+            percentage = 100.00
 
-        deactivate_wifi()
-        time.sleep(consts.SLEEP_INTERVAL_BETWEEN_MEASUREMENTS_SECS)
-        machine.reset()
-    except Exception as e:
-        print(f"Error in main loop: {e}")
-        import sys
-        sys.print_exception(e)
-        time.sleep(consts.SLEEP_INTERVAL_ON_ERROR_SECS)
-        machine.reset()
+        return percentage
 
+    def _disable_wifi(self):
+        if not self._spi_cs_control:
+            return
 
-if __name__ == "__main__":
-    main()
+        self._spi_cs_control.value(1)
+
+    def _enable_wifi(self):
+        if not self._spi_cs_control:
+            return
+
+        self._spi_cs_control.value(0)
+
+    def _init(self):
+        self._vsys_adc = machine.ADC(self.vsys_pin)  # reads the system input voltage
+
+        # machine.Pin.PULL_UP
+        self._spi_cs_control = machine.Pin(25, machine.Pin.OUT)
+        self._initialized = True
